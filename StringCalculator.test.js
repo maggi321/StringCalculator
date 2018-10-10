e174fb2da38bea5d23ec41456c493f78d441eb0e
maggi321 <magsterinn@gmail.com>
2018-10-10 16:27:50 +0000

    implement tekur 2 tölur og gefur summu

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -4,6 +4,10 @@ it("should return zero on an empty string", () =>  {
     expect(add("")).toBe(0);
 });
 
-it("should return zero on an empty string", () =>  {
+it("should return number only one number is in the string", () =>  {
     expect(add("7")).toBe(7);
+});
+
+it("should return sum of two given numbers", () =>  {
+    expect(add("4,6")).toBe(10);
 });
