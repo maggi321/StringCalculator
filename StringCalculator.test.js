a6ab75bdcec6631324d422011c085bb32b6364ea
maggi321 <magsterinn@gmail.com>
2018-10-11 19:47:58 +0000

    auka test bæt við og virkuðu öll í fyrstu tilraun

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -16,8 +16,16 @@ it("should sumerize multiple numbers", () =>  {
     expect(add("1,2,3")).toBe(6);
 });
 
+it("should sumerize multiple numbers", () =>  {
+    expect(add("1,2,3,4,5")).toBe(15);
+});
+
 it("should sumerize multiple numbers with a new line in string", () =>  {
     expect(add("1\n2,3")).toBe(6);
+});
+
+it("should sumerize multiple numbers with a new line in string", () =>  {
+    expect(add("1\n2\n3")).toBe(6);
 });
 
 it("negative number not allowed", () => {
@@ -25,6 +33,13 @@ it("negative number not allowed", () => {
         add("-1,2");
     }
     expect(trysum).toThrowError("Negatives not allowed: -1");
+});
+
+it("negative number not allowed", () => {
+    function trysum(){
+        add("-1,-2");
+    }
+    expect(trysum).toThrowError("Negatives not allowed: -1,-2");
 });
 
 it("negative number not allowed", () => {
@@ -38,6 +53,18 @@ it("should ignore numbers bigger then 1000", () =>  {
     expect(add("1001,2")).toBe(2);
 });
 
+it("should ignore numbers bigger then 1000", () =>  {
+    expect(add("1001,1002")).toBe(0);
+});
+
 it("should sumerize numbers with other delimiter", () =>  {
     expect(add("//;\n1;2")).toBe(3);
+});
+
+it("should sumerize numbers with other delimiter", () =>  {
+    expect(add("//{milli}\n2{milli}3")).toBe(5);
+});
+
+it("should sumerize numbers with other delimiter", () =>  {
+    expect(add("//test\n2test3test4")).toBe(9);
 });
